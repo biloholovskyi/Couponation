2ce8d45e5bfdf44cb182bb39d07cbeec0afbadf7
Biloholovskyi <biloholovskyi>
2020-06-18 20:16:29 +0300

    slider

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,16 @@
 import 'normalize.css';
 
 $(document).ready((e) => {
-
+  // main slider
+  $('.main-slider__body').owlCarousel({
+    loop: false,
+    nav: true,
+    navText: '',
+    dots: false,
+    autoplay: false,
+    margin: 10,
+    items: 5
+  });
 });
 
 $(window).resize(() => {
