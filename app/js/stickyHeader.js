b1bee2bae5a5549deca9fceb4c5f8cacae312775
Dychkevych Kolya <dychkevych.kolya@gmail.com>
2020-07-31 11:24:01 +0300

    fixes

--- app/js/stickyHeader.js
+++ app/js/stickyHeader.js
@@ -3,10 +3,12 @@ let scrolled;
 window.onscroll = function() {
   scrolled = window.pageYOffset || document.createElement.scrollTop;
   if( scrolled > 80 ) {
-    $('.header').addClass('active')
+    $('.header').addClass('active') 
+    $('.alphabet-single-wrap').addClass('active') 
   }
 
   if( 80 >scrolled ) {
-    $('.header').removeClass('active')  
+    $('.header').removeClass('active') 
+    $('.alphabet-single-wrap').removeClass('active')  
   }
 }
