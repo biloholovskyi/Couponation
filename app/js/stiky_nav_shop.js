f063fc2f2732abbf19857fcc461c66b55f80d45f
Dychkevych Kolya <dychkevych.kolya@gmail.com>
2020-08-08 15:36:22 +0300

    fixes

--- app/js/stiky_nav_shop.js
+++ app/js/stiky_nav_shop.js
@@ -1,33 +1,33 @@
-$(document).ready(function(){
-  let obj = $('#sticky');
-  let offset = obj.offset();
-  let topOffset = offset.top;
-  let marginTop = obj.css("marginTop");
+// $(document).ready(function(){
+//   let obj = $('#sticky');
+//   let offset = obj.offset();
+//   let topOffset = offset.top;
+//   let marginTop = obj.css("marginTop");
 
-  $(window).scroll(function() {
-  let scrollTop = $(window).scrollTop();
+//   $(window).scroll(function() {
+//   let scrollTop = $(window).scrollTop();
   
-    if (scrollTop >= topOffset){
-      obj.addClass('shadows');
-      obj.css({
-        marginTop: 0,
-        position: 'fixed', 
-        top: 63,
+//     if (scrollTop >= topOffset){
+//       obj.addClass('shadows');
+//       obj.css({
+//         marginTop: 0,
+//         position: 'fixed', 
+//         top: 63,
      
-      });
-      $('.header').removeClass('active');
-    }
+//       });
+//       $('.header').removeClass('active');
+//     }
   
-    if (scrollTop < topOffset){
-      obj.removeClass('shadows');
-      obj.css({
-        marginTop: 0,
-        position: 'relative',
-        top: 0,
+//     if (scrollTop < topOffset){
+//       obj.removeClass('shadows');
+//       obj.css({
+//         marginTop: 0,
+//         position: 'relative',
+//         top: 0,
       
         
-      });
-     $('.mobile__nav').find('.firstAnhors').addClass('active').siblings().removeClass('active');
-    }
-  });
-});
+//       });
+//      $('.mobile__nav').find('.firstAnhors').addClass('active').siblings().removeClass('active');
+//     }
+//   });
+// });
